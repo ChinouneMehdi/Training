007d1966ec74b5794b50f22241331df21e71849b
Mehdi Chinoune <chinoune.mehdi@hotmail.com>
2020-05-08 16:03:40 +0100

    Store extracted data in an hfd5 file.

--- hdf5/extract_hgt_gedi.f90
+++ hdf5/extract_hgt_gedi.f90
@@ -1,7 +1,9 @@
 program main
   use hdf5, only : h5open_f, h5close_f, h5gopen_f, h5gclose_f, h5fopen_f, &
     h5fclose_f, h5dopen_f, h5dclose_f, h5dread_f, h5f_acc_rdonly_f, h5t_ieee_f32le, &
-    h5t_ieee_f64le, hid_t, hsize_t, h5dget_space_f, h5sget_simple_extent_dims_f
+    h5t_ieee_f64le, hid_t, hsize_t, h5dget_space_f, h5sget_simple_extent_dims_f, &
+    h5screate_simple_f, h5sclose_f, h5dcreate_f, h5dwrite_f, h5gcreate_f, h5fcreate_f, &
+    h5f_acc_trunc_f
   implicit none
   !
   integer, parameter :: sp = selected_real_kind( 6, 37 )
@@ -11,14 +13,16 @@ program main
   real(dp), allocatable :: hr_lat(:), hr_lon(:), lm_lat(:), lm_lon(:)
   !
   character(len=*), parameter :: filename = "GEDI02_A_2019219212118_O03696_T01120_02_001_01.h5"
+  character(len=*), parameter :: filename2 = "data/gedi.h5"
   integer(hid_t) :: file_id, grp_id, dset_id, dspace_id
-  integer(hsize_t) :: dset_dim(1), maxdims(1), mt, j
+  integer(hid_t) :: file_id2, grp_id2, dset_id2, dspace_id2
+  integer(hsize_t) :: dset_dim(1), maxdims(1), mt
   character(:), allocatable :: grp_name
   character(4) :: beam_nb
   character(132) :: directory
   integer :: error
   !
-  integer :: hr_unit, lm_unit, i, dir_len
+  integer :: i, dir_len
   integer, parameter :: beam(0:7) = [ 0, 1, 2, 3, 5, 6, 8, 11 ]
   !
   ! Get Current Working Directory
@@ -30,13 +34,18 @@ program main
   call h5open_f(error)
   ! Open File
   call h5fopen_f( filename, h5f_acc_rdonly_f, file_id, error)
+  ! Create Output File
+  call h5fcreate_f( filename2, h5f_acc_trunc_f, file_id2, error)
   !
   do i = 0, 7
     write( beam_nb, '(b4.4)' ) beam(i)
-    ! Open Group
     grp_name = "BEAM"//beam_nb
+    ! Open Group
     call h5gopen_f(file_id, grp_name, grp_id, error )
-    ! highest return elvation
+    ! Create Output Group
+    call h5gcreate_f(file_id2, grp_name, grp_id2, error )
+    ! highest return elevation
+    ! Get Data
     call h5dopen_f(grp_id, "elev_highestreturn", dset_id, error )
     ! Get dimensions
     call h5dget_space_f(dset_id, dspace_id, error )
@@ -47,51 +56,83 @@ program main
     ! Continue
     call h5dread_f(dset_id, h5t_ieee_f32le, highestreturn, dset_dim, error)
     call h5dclose_f(dset_id, error)
-    ! latitude
+    ! Write Data
+    call h5screate_simple_f( 1, dset_dim, dspace_id2, error )
+    call h5dcreate_f(grp_id2, "elev_highestreturn", h5t_ieee_f32le, dspace_id2, &
+       dset_id2, error)
+    call h5dwrite_f(dset_id2, h5t_ieee_f32le, highestreturn, dset_dim, error)
+    call h5dclose_f(dset_id2, error)
+    CALL h5sclose_f(dspace_id2, error)
+    ! Latitude
+    ! Get Data
     call h5dopen_f(grp_id, "lat_highestreturn", dset_id, error )
     call h5dread_f(dset_id, h5t_ieee_f64le, hr_lat, dset_dim, error)
     call h5dclose_f(dset_id, error)
-    ! longitude
+    ! Write Data
+    call h5screate_simple_f( 1, dset_dim, dspace_id2, error )
+    call h5dcreate_f(grp_id2, "lat_highestreturn", h5t_ieee_f64le, dspace_id2, &
+       dset_id2, error)
+    call h5dwrite_f(dset_id2, h5t_ieee_f64le, hr_lat, dset_dim, error)
+    call h5dclose_f(dset_id2, error)
+    CALL h5sclose_f(dspace_id2, error)
+    ! Longitude
+    ! Get Data
     call h5dopen_f(grp_id, "lon_highestreturn", dset_id, error )
     call h5dread_f(dset_id, h5t_ieee_f64le, hr_lon, dset_dim, error)
     call h5dclose_f(dset_id, error)
-    ! lowest mode elevation
+    ! Write Data
+    call h5screate_simple_f( 1, dset_dim, dspace_id2, error )
+    call h5dcreate_f(grp_id2, "lon_highestreturn", h5t_ieee_f64le, dspace_id2, &
+       dset_id2, error)
+    call h5dwrite_f(dset_id2, h5t_ieee_f64le, hr_lon, dset_dim, error)
+    call h5dclose_f(dset_id2, error)
+    call h5sclose_f(dspace_id2, error)
+    ! Lowest mode elevation
+    ! Get Data
     call h5dopen_f(grp_id, "elev_lowestmode", dset_id, error )
     call h5dread_f(dset_id, h5t_ieee_f32le, lowestmode, dset_dim, error)
     call h5dclose_f(dset_id, error)
-    ! latitude
+    ! Write Data
+    call h5screate_simple_f( 1, dset_dim, dspace_id2, error )
+    call h5dcreate_f(grp_id2, "elev_lowestmode", h5t_ieee_f32le, dspace_id2, &
+       dset_id2, error)
+    call h5dwrite_f(dset_id2, h5t_ieee_f32le, lowestmode, dset_dim, error)
+    call h5dclose_f(dset_id2, error)
+    call h5sclose_f(dspace_id2, error)
+    ! Latitude
+    ! Get Data
     call h5dopen_f(grp_id, "lat_lowestmode", dset_id, error )
     call h5dread_f(dset_id, h5t_ieee_f64le, lm_lat, dset_dim, error)
     call h5dclose_f(dset_id, error)
-    ! longitude
+    ! Write Data
+    call h5screate_simple_f( 1, dset_dim, dspace_id2, error )
+    call h5dcreate_f(grp_id2, "lat_lowestmode", h5t_ieee_f64le, dspace_id2, &
+       dset_id2, error)
+    call h5dwrite_f(dset_id2, h5t_ieee_f64le, lm_lat, dset_dim, error)
+    call h5dclose_f(dset_id2, error)
+    call h5sclose_f(dspace_id2, error)
+    ! Longitude
+    ! Get Data
     call h5dopen_f(grp_id, "lon_lowestmode", dset_id, error )
     call h5dread_f(dset_id, h5t_ieee_f64le, lm_lon, dset_dim, error)
     call h5dclose_f(dset_id, error)
-    ! Close Group
+    ! Write Data
+    call h5screate_simple_f( 1, dset_dim, dspace_id2, error )
+    call h5dcreate_f(grp_id2, "lon_lowestmode", h5t_ieee_f64le, dspace_id2, &
+       dset_id2, error)
+    call h5dwrite_f(dset_id2, h5t_ieee_f64le, lm_lon, dset_dim, error)
+    call h5dclose_f(dset_id2, error)
+    call h5sclose_f(dspace_id2, error)
+    ! Close Groups
     call h5gclose_f(grp_id, error)
-    ! Open Output Files
-    open( newunit = hr_unit, file = directory(1:dir_len)//"/data/highestreturn"//beam_nb//".dat", &
-      action = "write", status = "replace", form = "unformatted" )
-    open( newunit = lm_unit, file = directory(1:dir_len)//"/data/lowestmode"//beam_nb//".dat", &
-      action = "write", status = "replace", form = "unformatted" )
-    ! Write Extracted Data
-    do j = 1, mt
-      !if( hr_lat(j)>=35._dp .and. hr_lat(j)<=36._dp .and. hr_lon(j)>=5._dp &
-      !  .and. hr_lon(j)<=6._dp ) &
-        write( hr_unit ) hr_lon(j), hr_lat(j), highestreturn(j)
-      !if( lm_lat(j)>=35._dp .and. lm_lat(j)<=36._dp .and. lm_lon(j)>=5._dp &
-      !  .and. lm_lon(j)<=6._dp ) &
-        write( lm_unit ) lm_lon(j), lm_lat(j), lowestmode(j)
-    end do
-    ! Close Output Files
-    close( hr_unit )
-    close( lm_unit )
+    call h5gclose_f(grp_id2, error)
     !
     deallocate( highestreturn, lowestmode, hr_lat, hr_lon, lm_lat, lm_lon )
     !
   end do
-  ! Close File
+  ! Close Files
   call h5fclose_f(file_id, error)
+  call h5fclose_f(file_id2, error)
   ! Close Fortran Interface
   call h5close_f(error)
   !
